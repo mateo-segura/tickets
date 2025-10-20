46f633f11edff312d5c46fb0b2a3993145402a27
Mateo <a01425792@tec.mx>
2025-10-20 11:25:29 -0600

    Agregar unversioned

---
+++ app/src/main/java/mx/tec/tickets/ui/screens/mesa/components/MesaTicketList.kt
@@ -0,0 +1,92 @@
+package mx.tec.tickets.ui.screens.mesa.components
+
+import android.content.Context
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import mx.tec.tickets.model.CommonTicket
+import mx.tec.tickets.model.NonAcceptedTicket
+import mx.tec.tickets.ui.screens.tecnico.components.TecnicoAcceptedTicketCard
+import org.json.JSONArray
+
+@Composable
+fun MesaTicketList(navController: NavController, userID: Int,token:String) {
+    val context = LocalContext.current
+    var Tickets by remember { mutableStateOf(listOf<CommonTicket>()) }
+
+    LaunchedEffect(Unit) {
+        fetchTickets(context, userID,token) { fetchedTickets ->
+            Tickets = fetchedTickets
+        }
+    }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+        //.statusBarsPadding()
+        // Status bars padding estaba causando padding conflictivo
+    ) {
+        items(Tickets) { ticket ->
+            MesaTicketCard(ticket, navController, userID)
+        }
+    }
+}
+
+
+fun fetchTickets(
+    context: Context,
+    userID: Int,
+    token: String,
+    onResult: (List<CommonTicket>) -> Unit
+) {
+    val tickets = mutableListOf<CommonTicket>()
+    val queue = Volley.newRequestQueue(context)
+    val url = "http://10.0.2.2:3000/tickets"
+    val metodo = Request.Method.GET
+    val listener = Response.Listener<JSONArray> { response ->
+        for (i in 0 until response.length()) {
+            val acceptedTickets = CommonTicket(
+                response.getJSONObject(i).getInt("id"),
+                response.getJSONObject(i).getString("title"),
+                response.getJSONObject(i).getString("description"),
+                response.getJSONObject(i).getString("category"),
+                response.getJSONObject(i).getString("priority"),
+                response.getJSONObject(i).getString("status"),
+                response.getJSONObject(i).getInt("created_by"),
+                response.getJSONObject(i).getInt("assigned_to"),
+                response.getJSONObject(i).getString("created_at"),
+                response.getJSONObject(i).getInt("is_active"),
+                response.getJSONObject(i).getInt("acepted"),
+
+                )
+            tickets.add(acceptedTickets)
+        }
+        onResult(tickets)
+    }
+    val errorListener = Response.ErrorListener { error ->
+        Log.e("VolleyError", error.message.toString())
+    }
+    val request = object : JsonArrayRequest(
+        metodo, url, null, listener, errorListener
+    ) {
+        override fun getHeaders(): MutableMap<String, String> {
+            val headers = HashMap<String, String>()
+            headers["Authorization"] = "Bearer $token"
+            return headers
+        }
+    }
+    queue.add(request)
+}
