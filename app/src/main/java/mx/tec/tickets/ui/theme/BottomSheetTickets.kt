292d1486b2a8df9a1605d70acd5fd6e552d3b70c
Andres Peña <A01029309@tec.mx>
2025-10-20 23:33:57 -0600

    Funcionalidad con middleware faltante

--- app/src/main/java/mx/tec/tickets/ui/theme/BottomSheetTickets.kt
+++ app/src/main/java/mx/tec/tickets/ui/theme/BottomSheetTickets.kt
@@ -41,16 +41,18 @@ fun BottomSheetTickets(
 
 ) {
     val context = LocalContext.current
-    val (ticket, setTicket) = remember { mutableStateOf<Ticket?>(null)}
+    // val (ticket, setTicket) = remember { mutableStateOf<Ticket?>(null)}
 
     val sheetState = rememberModalBottomSheetState(
         skipPartiallyExpanded = true // optional, prevents half-expanded state
     )
+    /*
     LaunchedEffect(Unit){
         fetchTicket(context) { result ->
             setTicket(result)
         }
     }
+     */
 
     // Bottom sheet itself
     if (showSheet) {
@@ -63,13 +65,8 @@ fun BottomSheetTickets(
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
-                if (ticket != null){
-                    Text(text = "Titulo: ${nonAcceptedTicket.title}",
-                        style = MaterialTheme.typography.titleLarge)
-                } else {
-                    Text(text = "Titulo: Error",
-                        style = MaterialTheme.typography.titleLarge)
-                }
+                Text(text = "Titulo: ${nonAcceptedTicket.title}",
+                    style = MaterialTheme.typography.titleLarge)
 
                 Row(
                     modifier = Modifier.padding(8.dp),
@@ -80,17 +77,11 @@ fun BottomSheetTickets(
                             .background(Color.Red) // Cambiar color
                             .size(20.dp)
                     )
-                    if (ticket != null) {
-                        Text(
-                            modifier = Modifier.padding(horizontal = 16.dp),
-                            text = "Prioridad - ${nonAcceptedTicket.priority}"
-                        )
-                    } else {
-                        Text(
-                            modifier = Modifier.padding(horizontal = 16.dp),
-                            text = "Prioridad - Error"
-                        )
-                    }
+
+                    Text(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        text = "Prioridad - ${nonAcceptedTicket.priority}"
+                    )
                 }
                 Row(
                     modifier = Modifier.padding(8.dp),
@@ -101,17 +92,11 @@ fun BottomSheetTickets(
                             .background(Color.Green) // Cambiar el color
                             .size(20.dp)
                     )
-                    if (ticket != null) {
-                        Text(
-                            modifier = Modifier.padding(horizontal = 16.dp),
-                            text = "Categoria - ${nonAcceptedTicket.category}"
-                        )
-                    } else {
-                        Text(
-                            modifier = Modifier.padding(horizontal = 16.dp),
-                            text = "Categoria - Error"
-                        )
-                    }
+
+                    Text(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        text = "Categoria - ${nonAcceptedTicket.category}"
+                    )
                 }
 
                 Spacer(modifier = Modifier.height(8.dp))
@@ -131,17 +116,10 @@ fun BottomSheetTickets(
                         .verticalScroll(rememberScrollState())
                         .padding(horizontal = 4.dp, vertical = 16.dp)
                 ){
-                    if (ticket != null) {
-                        Text(
-                            text = nonAcceptedTicket.description,
-                            style = MaterialTheme.typography.bodyMedium
-                        )
-                    } else {
-                        Text(
-                            text = "Error",
-                            style = MaterialTheme.typography.bodyMedium
-                        )
-                    }
+                    Text(
+                        text = nonAcceptedTicket.description,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
                 }
 
                 if (!new) {
@@ -193,40 +171,6 @@ fun BottomSheetTickets(
             }
         }
     }
-}
-
-fun fetchTicket(context: Context, onResult: (Ticket?) -> Unit) {
-    val url = "http://10.0.2.2:3000/tickets/1"
-    val queue = Volley.newRequestQueue(context)
-
-    val request = JsonObjectRequest(
-        Request.Method.GET,
-        url,
-        null,
-        { response ->
-            try {
-                // Parse response
-                val ticket = Ticket(
-                    title = response.getString("title"),
-                    priority = response.getString("priority"),
-                    assignedTo = response.getInt("assigned_to"),
-                    category = response.getString("category"),
-                    createdAt = response.getString("created_at"),
-                    description = response.getString("description")
-                )
-                onResult(ticket) // Return ticket
-            } catch (e: Exception) {
-                Log.e("VolleyParseError", e.message.toString())
-                onResult(null)
-            }
-        },
-        { error ->
-            Log.e("VolleyError", error.message.toString())
-            onResult(null)
-        }
-    )
-
-    queue.add(request)
 }
 
 fun acceptTicket(context: Context, jwtToken: String, ticketId: Int, onSuccess: () -> Unit) {
